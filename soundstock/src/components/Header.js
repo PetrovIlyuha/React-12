c55a26de508de1495d884be73380aa4cdfd4d4b7
Ilya Petrov <dnamix1@gmail.com>
2020-04-13 10:48:59 +0300

    logo image and header component

---
+++ soundstock/src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Logo from "../Logo";
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginLeft: theme.spacing(2),
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <AppBar
+      style={{
+        backgroundColor: "#00DBDE",
+        backgroundImage: "linear-gradient(90deg, #303242 0%, #004A2F 100%)",
+      }}
+      position="fixed"
+    >
+      <Toolbar>
+        <Logo width="60px" />
+        <Typography variant="h6" component="h1" className={classes.title}>
+          SoundStock
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
