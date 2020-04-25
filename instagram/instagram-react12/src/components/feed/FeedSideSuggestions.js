54b2aae05eb7233d690aad052441bdb5ed561145
Ilya Petrov <dnamix1@gmail.com>
2020-04-25 12:07:14 +0300

    side feed suggestionsfilled with default users

--- instagram/instagram-react12/src/components/feed/FeedSideSuggestions.js
+++ instagram/instagram-react12/src/components/feed/FeedSideSuggestions.js
@@ -1,10 +1,42 @@
 import React from "react";
 import { useFeedSideSuggestionsStyles } from "../../styles";
+import { Paper, Typography } from "@material-ui/core";
+import { getDefaultUser } from "../../data";
+import UserCard from "../shared/UserCard";
+import FollowButton from "../shared/FollowButton";
+import { LoadingIcon } from "../../icons";
 
 function FeedSideSuggestions() {
-  useFeedSideSuggestionsStyles();
+  const classes = useFeedSideSuggestionsStyles();
 
-  return <div>FeedSideSuggestions</div>;
+  let loading = true;
+
+  return (
+    <article className={classes.article}>
+      <Paper className={classes.paper}>
+        <Typography
+          variant="subtitle2"
+          color="textSecondary"
+          component="h2"
+          align="left"
+          gutterBottom
+          className={classes.typography}
+        >
+          Suggestions for you
+        </Typography>
+        {loading ? (
+          <LoadingIcon />
+        ) : (
+          Array.from({ length: 5 }, () => getDefaultUser()).map((user) => (
+            <div key={user.id} className={classes.card}>
+              <UserCard user={user} />
+              <FollowButton side />
+            </div>
+          ))
+        )}
+      </Paper>
+    </article>
+  );
 }
 
 export default FeedSideSuggestions;
