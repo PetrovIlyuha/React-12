55e802c8c39007675f247874d32195d83db7bce4
Ilya Petrov <dnamix1@gmail.com>
2020-04-25 12:10:11 +0300

    posts feed filled with default posts from local dataset

--- instagram/instagram-react12/src/pages/feed.js
+++ instagram/instagram-react12/src/pages/feed.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFeedPageStyles } from "../styles";
+import Layout from "../components/shared/Layout";
+import FeedPost from "../components/feed/FeedPost";
+import UserCard from "../components/shared/UserCard";
+import LoadingScreen from "../components/shared/LoadingScreen";
+import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
+import { Hidden } from "@material-ui/core";
+import { getDefaultPost } from "../data";
+import { LoadingLargeIcon } from "../icons";
 
 function FeedPage() {
-  useFeedPageStyles();
-
-  return <div>FeedPage</div>;
+  const classes = useFeedPageStyles();
+  const [isEndOfFeed, setIsEndOfFeed] = useState(false);
+  let loading = false;
+  if (loading) return <LoadingScreen />;
+  return (
+    <Layout>
+      <div className={classes.container}>
+        {/* Feed Post */}
+        <div>
+          {Array.from({ length: 5 }, () => getDefaultPost()).map((post) => (
+            <FeedPost key={post.id} post={post} />
+          ))}
+        </div>
+        {/* SideBar */}
+        <Hidden smDown>
+          <div className={classes.sidebarContainer}>
+            <div className={classes.sidebarWrapper}>
+              <UserCard avatarSize={50} />
+              <FeedSideSuggestions />
+            </div>
+          </div>
+        </Hidden>
+        {!isEndOfFeed && <LoadingLargeIcon />}
+      </div>
+    </Layout>
+  );
 }
 
 export default FeedPage;
