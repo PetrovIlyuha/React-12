3868efdd136b012aa38d351e0a58364ff61fcfe5
Ilya Petrov <dnamix1@gmail.com>
2020-04-25 12:06:18 +0300

    feed post page layed out

--- instagram/instagram-react12/src/components/feed/FeedPost.js
+++ instagram/instagram-react12/src/components/feed/FeedPost.js
@@ -1,10 +1,192 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import {
+  MoreIcon,
+  CommentIcon,
+  ShareIcon,
+  UnlikeIcon,
+  LikeIcon,
+  RemoveIcon,
+  SaveIcon,
+} from "../../icons";
+import { Link } from "react-router-dom";
+import {
+  Typography,
+  Button,
+  Hidden,
+  Divider,
+  TextField,
+} from "@material-ui/core";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const [showCaption, setShowCaption] = useState(false);
+  const { media, id, likes, user, caption, comments } = post;
+  return (
+    <React.Fragment>
+      <article className={classes.article}>
+        {/* Feed Post Header */}
+        <div className={classes.postHeader}>
+          <UserCard user={user} />
+          <MoreIcon className={classes.MoreIcon} />
+        </div>
+        {/* Feed Post Image */}
+        <div>
+          <img src={media} alt="Post Media" className={classes.image} />
+        </div>
+        {/* Feed Post buttons */}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            <LikeButton />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            <SaveButton />
+          </div>
+          <Typography className={classes.likes} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+          <div className={showCaption ? classes.expanded : classes.collapsed}>
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            {showCaption ? (
+              <Typography
+                variant="body2"
+                component="span"
+                dangerouslySetInnerHTML={{ __html: caption }}
+              />
+            ) : (
+              <div className={classes.captionWrapper}>
+                <HTMLEllipsis
+                  unsafeHTML={caption}
+                  className={classes.caption}
+                  maxLine="0"
+                  ellipsis="..."
+                  basedOn="letters"
+                />
+                <Button
+                  className={classes.moreButton}
+                  onClick={() => setShowCaption(true)}
+                >
+                  More
+                </Button>
+              </div>
+            )}
+          </div>
+          <Link to={`/p/${id}`}>
+            <Typography
+              className={classes.commentsLink}
+              variant="body2"
+              component="div"
+            >
+              View All {comments.length} comments
+            </Typography>
+          </Link>
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <Link to={`/${comment.user.username}`}>
+                <Typography
+                  variant="subtitle2"
+                  component="span"
+                  className={classes.commentUserName}
+                >
+                  {comment.user.username}
+                </Typography>
+                {""}
+                <Typography variant="body2" component="span">
+                  {comment.content}
+                </Typography>
+              </Link>
+            </div>
+          ))}
+          <Typography color="textSecondary" className={classes.datePosted}>
+            5 Days Ago
+          </Typography>
+        </div>
+        <Hidden xsDown>
+          <Divider />
+          <Comment />
+        </Hidden>
+      </article>
+    </React.Fragment>
+  );
+}
 
-  return <div>FeedPost</div>;
+function LikeButton() {
+  const classes = useFeedPostStyles();
+  const [liked, setLiked] = useState(false);
+  const Icon = liked ? UnlikeIcon : LikeIcon;
+  const className = liked ? classes.liked : classes.like;
+  const onClick = liked ? handleUnlike : handleLike;
+
+  function handleLike() {
+    setLiked(true);
+  }
+
+  function handleUnlike() {
+    setLiked(false);
+  }
+
+  return <Icon className={className} onClick={onClick} />;
+}
+
+function SaveButton() {
+  const classes = useFeedPostStyles();
+  const [saved, setSaved] = useState(false);
+  const Icon = saved ? RemoveIcon : SaveIcon;
+  const onClick = saved ? handleRemove : handleSave;
+
+  function handleRemove() {
+    setSaved(false);
+  }
+
+  function handleSave() {
+    setSaved(true);
+  }
+
+  return <Icon className={classes.saveIcon} onClick={onClick} />;
+}
+
+function Comment() {
+  const classes = useFeedPostStyles();
+  const [content, setContent] = useState("");
+  return (
+    <div className={classes.commentContainer}>
+      <TextField
+        fullWidth
+        value={content}
+        placeholder="Add a comment..."
+        multiline
+        rowsMax={2}
+        rows={1}
+        className={classes.textField}
+        onChange={(e) => setContent(e.target.value)}
+        InputProps={{
+          classes: {
+            root: classes.root,
+            underline: classes.underline,
+          },
+        }}
+      />
+      <Button
+        color="primary"
+        className={classes.commentButton}
+        disabled={!content.trim()}
+      >
+        Post
+      </Button>
+    </div>
+  );
 }
 
 export default FeedPost;
