48c7d7c1712de82622ab6aef110d001fd65d841c
Ilya Petrov <dnamix1@gmail.com>
2020-04-26 11:54:24 +0300

    passing down prop for minimization of navbar UI from Layout to NavBar

--- instagram/instagram-react12/src/data.js
+++ instagram/instagram-react12/src/data.js
@@ -65,8 +65,7 @@ export const defaultCurrentUser = {
   username: "me",
   name: "myself",
   profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
-  website: "https://react12.io",
+    "http://latitudes-sample.com/wp-content/uploads/2016/03/Miniclip-8-Ball-Pool-Avatar-8-300x300.png",
   email: "me@gmail.com",
   bio: "This is my bio",
   phone_number: "555-555-5555",
