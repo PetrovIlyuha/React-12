31cef90873539ddc0e1f1fd23f6bb6a490fb4983
Ilya Petrov <dnamix1@gmail.com>
2020-04-27 13:25:29 +0300

    exploreSuggestions layout

--- instagram/instagram-react12/src/components/explore/ExploreSuggestions.js
+++ instagram/instagram-react12/src/components/explore/ExploreSuggestions.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { useExploreSuggestionsStyles } from "../../styles";
+import { Hidden, Typography } from "@material-ui/core";
+import FollowSuggestions from "../shared/FollowSuggestions";
 
 function ExploreSuggestions() {
-  useExploreSuggestionsStyles();
+  const classes = useExploreSuggestionsStyles();
 
-  return <div>ExploreSuggestions</div>;
+  return (
+    <Hidden xsDown>
+      <div className={classes.container}>
+        <Typography
+          variant="subtitle2"
+          color="textSecondary"
+          component="h2"
+          className={classes.typography}
+        >
+          Discover People
+        </Typography>
+        <FollowSuggestions hideHeader />
+      </div>
+    </Hidden>
+  );
 }
 
 export default ExploreSuggestions;
