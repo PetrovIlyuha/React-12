4439f7a266e5a636c0508e5092232550f746b700
Ilya Petrov <dnamix1@gmail.com>
2020-04-28 13:13:58 +0900

    single post with more posts from the same user recommendation feed below the main post area

--- instagram/instagram-react12/src/components/post/Post.js
+++ instagram/instagram-react12/src/components/post/Post.js
@@ -1,10 +1,168 @@
-import React from "react";
+import React, { useState } from "react";
 import { usePostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import {
+  MoreIcon,
+  CommentIcon,
+  ShareIcon,
+  UnlikeIcon,
+  LikeIcon,
+  RemoveIcon,
+  SaveIcon,
+} from "../../icons";
+import { Link } from "react-router-dom";
+import {
+  Typography,
+  Button,
+  Hidden,
+  Divider,
+  TextField,
+} from "@material-ui/core";
+import OptionsDialog from "../shared/OptionsDialog";
+import { defaultPost } from "../../data";
 
 function Post() {
-  usePostStyles();
+  const classes = usePostStyles();
+  const [showOptionsDialog, setOptionsDialog] = useState(false);
+  const { media, id, likes, user, caption, comments } = defaultPost;
 
-  return <div>Post</div>;
+  return (
+    <div className={classes.postContainer}>
+      <article className={classes.article}>
+        {/* Post Header */}
+        <div className={classes.postHeader}>
+          <UserCard user={user} avatarSize={32} />
+          <MoreIcon
+            className={classes.moreIcon}
+            style={{ cursor: "pointer" }}
+            onClick={() => setOptionsDialog(true)}
+          />
+        </div>
+        {/* Post Image */}
+        <div className={classes.postImage}>
+          <img src={media} alt="Post Media" className={classes.image} />
+        </div>
+        {/* Post buttons */}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            <LikeButton />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            <SaveButton />
+          </div>
+          <Typography className={classes.likes} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+          <div className={classes.postCaptionContainer}>
+            <Typography
+              variant="body2"
+              component="span"
+              className={classes.postCaption}
+              dangerouslySetInnerHTML={{ __html: caption }}
+            />
+            {comments.map((comment) => (
+              <div key={comment.id}>
+                <Link to={`/${comment.user.username}`}>
+                  <Typography
+                    variant="subtitle2"
+                    component="span"
+                    className={classes.commentUserName}
+                  >
+                    {comment.user.username}
+                  </Typography>
+                  {""}
+                  <Typography variant="body2" component="span">
+                    {comment.content}
+                  </Typography>
+                </Link>
+              </div>
+            ))}
+          </div>
+          <Typography color="textSecondary" className={classes.datePosted}>
+            5 Days Ago
+          </Typography>
+          <Hidden xsDown>
+            <div className={classes.commentContainer}>
+              <Divider />
+              <Comment />
+            </div>
+          </Hidden>
+        </div>
+      </article>
+      {showOptionsDialog && (
+        <OptionsDialog onClose={() => setOptionsDialog(false)} />
+      )}
+    </div>
+  );
+}
+
+function LikeButton() {
+  const classes = usePostStyles();
+  const [liked, setLiked] = useState(false);
+  const Icon = liked ? UnlikeIcon : LikeIcon;
+  const className = liked ? classes.liked : classes.like;
+  const onClick = liked ? handleUnlike : handleLike;
+
+  function handleLike() {
+    setLiked(true);
+  }
+
+  function handleUnlike() {
+    setLiked(false);
+  }
+
+  return <Icon className={className} onClick={onClick} />;
+}
+
+function SaveButton() {
+  const classes = usePostStyles();
+  const [saved, setSaved] = useState(false);
+  const Icon = saved ? RemoveIcon : SaveIcon;
+  const onClick = saved ? handleRemove : handleSave;
+
+  function handleRemove() {
+    setSaved(false);
+  }
+
+  function handleSave() {
+    setSaved(true);
+  }
+
+  return <Icon className={classes.saveIcon} onClick={onClick} />;
+}
+
+function Comment() {
+  const classes = usePostStyles();
+  const [content, setContent] = useState("");
+  return (
+    <div className={classes.commentContainer}>
+      <TextField
+        fullWidth
+        value={content}
+        placeholder="Add a comment..."
+        multiline
+        rowsMax={2}
+        rows={1}
+        className={classes.textField}
+        onChange={(e) => setContent(e.target.value)}
+        InputProps={{
+          classes: {
+            root: classes.root,
+            underline: classes.underline,
+          },
+        }}
+      />
+      <Button
+        color="primary"
+        className={classes.commentButton}
+        disabled={!content.trim()}
+      >
+        Post
+      </Button>
+    </div>
+  );
 }
 
 export default Post;
