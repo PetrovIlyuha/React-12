6c9aef8c560e26729094d732acf9fd22f68bd0b9
Ilya Petrov <dnamix1@gmail.com>
2020-04-29 11:28:34 +0300

    removed unneccessary dependencies from feed component

--- instagram/instagram-react12/src/pages/feed.js
+++ instagram/instagram-react12/src/pages/feed.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import { useFeedPageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
-import FeedPost from "../components/feed/FeedPost";
 import UserCard from "../components/shared/UserCard";
 import LoadingScreen from "../components/shared/LoadingScreen";
 import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
 import { Hidden } from "@material-ui/core";
 import { getDefaultPost } from "../data";
 import { LoadingLargeIcon } from "../icons";
+import FeedPostSkeleton from "../components/feed/FeedPostSkeleton";
+const FeedPost = React.lazy(() => import("../components/feed/FeedPost"));
 
 function FeedPage() {
   const classes = useFeedPageStyles();
-  const [isEndOfFeed, setIsEndOfFeed] = useState(false);
+  const [isEndOfFeed] = useState(false);
   let loading = false;
   if (loading) return <LoadingScreen />;
   return (
@@ -21,7 +22,9 @@ function FeedPage() {
         <div>
           {Array.from({ length: 5 }, () => getDefaultPost()).map(
             (post, index) => (
-              <FeedPost index={index} key={post.id} post={post} />
+              <React.Suspense fallback={<FeedPostSkeleton />}>
+                <FeedPost index={index} key={post.id} post={post} />
+              </React.Suspense>
             )
           )}
         </div>
