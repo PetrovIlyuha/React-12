7a1af16130f442162b1ceaaa87721bd973833cc5
Ilya Petrov <dnamix1@gmail.com>
2020-04-29 11:26:23 +0300

    fully formed profile picture for the edit-profile page

--- instagram/instagram-react12/src/components/shared/ProfilePicture.js
+++ instagram/instagram-react12/src/components/shared/ProfilePicture.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { useProfilePictureStyles } from "../../styles";
+import { Person } from "@material-ui/icons";
 
-function ProfilePicture() {
-  useProfilePictureStyles();
+function ProfilePicture({
+  size,
+  image = "http://latitudes-sample.com/wp-content/uploads/2016/03/Miniclip-8-Ball-Pool-Avatar-8-300x300.png",
+  isOwner,
+}) {
+  const classes = useProfilePictureStyles({ size, isOwner });
 
-  return <div>ProfilePicture</div>;
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt="User Profile" className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ProfilePicture;
