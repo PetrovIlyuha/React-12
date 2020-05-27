efc5373de26d2495fd1819cf1744409c3a37bed0
Ilya Petrov <dnamix1@gmail.com>
2020-05-27 17:12:21 +0600

    firebase and hasura auth setup (cloud function spawned)

---
+++ instagram/instagram-react12/src/auth.js
@@ -0,0 +1,105 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import React, { useState, useEffect } from "react";
+import defaultUserImage from "./images/default-user-image.jpg";
+import { useMutation } from "@apollo/react-hooks";
+import { CREATE_USER } from "./graphql/mutations";
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Find these options in your Firebase console
+firebase.initializeApp({
+  apiKey: "AIzaSyBUz-acdje4kGmtRN47WzaAXpXVVDpacB8",
+  authDomain: "instagram-react-a3994.firebaseapp.com",
+  databaseURL: "https://instagram-react-a3994.firebaseio.com",
+  projectId: "instagram-react-a3994",
+  storageBucket: "instagram-react-a3994.appspot.com",
+  messagingSenderId: "129771867158",
+  appId: "1:129771867158:web:28adef37d2d0d480177937",
+  measurementId: "G-3SXNPZPGCC",
+});
+
+export const AuthContext = React.createContext();
+
+function AuthProvider({ children }) {
+  const [authState, setAuthState] = useState({ status: "loading" });
+  const [createUser] = useMutation(CREATE_USER);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const token = await user.getIdToken();
+        const idTokenResult = await user.getIdTokenResult();
+        const hasuraClaim =
+          idTokenResult.claims["https://hasura.io/jwt/claims"];
+
+        if (hasuraClaim) {
+          setAuthState({ status: "in", user, token });
+        } else {
+          // Check if refresh is required.
+          const metadataRef = firebase
+            .database()
+            .ref(`metadata/${user.uid}/refreshTime`);
+
+          metadataRef.on("value", async (data) => {
+            if (!data.exists) return;
+            // Force refresh to pick up the latest custom claims changes.
+            const token = await user.getIdToken(true);
+            setAuthState({ status: "in", user, token });
+          });
+        }
+      } else {
+        setAuthState({ status: "out" });
+      }
+    });
+  }, []);
+
+  async function signInWithGoogle() {
+    await firebase.auth().signInWithPopup(provider);
+  }
+
+  async function signUpWithEmailAndPassword(formData) {
+    const data = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(formData.email, formData.password);
+    if (data.additionalUserInfo.isNewUser) {
+      const variables = {
+        userId: data.user.uid,
+        name: formData.name,
+        username: formData.username,
+        email: data.user.email,
+        bio: "",
+        website: "",
+        phoneNumber: "",
+        profileImage: defaultUserImage,
+      };
+      await createUser({ variables });
+    }
+  }
+
+  async function signOut() {
+    setAuthState({ status: "loading" });
+    await firebase.auth().signOut();
+    setAuthState({ status: "out" });
+  }
+
+  if (authState.status === "loading") {
+    return null;
+  } else {
+    return (
+      <AuthContext.Provider
+        value={{
+          authState,
+          signInWithGoogle,
+          signUpWithEmailAndPassword,
+          signOut,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    );
+  }
+}
+
+export default AuthProvider;
