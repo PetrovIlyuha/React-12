5df91e50f0ca36e75583dfec488ba260a3a0feb4
Ilya Petrov <dnamix1@gmail.com>
2020-05-29 12:48:37 +0300

    profile editing menu log out option is functional

--- instagram/instagram-react12/src/pages/profile.js
+++ instagram/instagram-react12/src/pages/profile.js
@@ -16,9 +16,10 @@ import {
   Typography,
   Zoom,
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { GearIcon } from "../icons";
 import ProfileTabs from "../components/profile/ProfileTabs";
+import { AuthContext } from "../auth";
 
 function ProfilePage() {
   const classes = useProfilePageStyles();
@@ -242,10 +243,16 @@ function NameBioSection({ user }) {
 
 function OptionsMenu({ handleCloseMenu }) {
   const classes = useProfilePageStyles();
+  const { signOut } = React.useContext(AuthContext);
   const [showLogOutMessage, setLogOutMessage] = useState(false);
+  const history = useHistory();
 
   function handleLogOutClick() {
     setLogOutMessage(true);
+    setTimeout(() => {
+      signOut();
+      history.push("/accounts/login");
+    }, 2000);
   }
 
   return (
